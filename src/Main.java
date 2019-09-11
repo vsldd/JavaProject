ec3358e648bb12122674ae33a0b6f348d4b38548
Vasile Doda <vdoda90@gmail.com>
2019-09-11 20:49:01 +0300

    hbi

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+        // Tipuri de date:
+
+        String masina = "Audi";
+        String culoare = "gri";
+        int vitezaMaxima = 320;
+        int greutate = 1700;
+        byte nrViteze = 6;
+        int kilometraj = 80000;
+        byte distantaJob = 5;
+        float consumMediu = 9f;
+
+        double consumJob = distantaJob * (0.01 * consumMediu);
+
+        System.out.println(consumJob);
+
+    }
+}
